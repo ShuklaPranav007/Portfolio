05d1e3ae65ab3e9b36c9624da4ed663508c1ce4a
ShuklaPranav007 <shuklapranav739@gmail.com>
2025-08-22 04:36:24 +0530

    skills section

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 import Header from './components/header/header.jsx'
 import Home from './components/home/home.jsx'
 import About from './components/home/about.jsx'
+import Skills from './components/skills/skills.jsx'
+
 function App() {
   return (
     <>
@@ -10,6 +12,7 @@ function App() {
       <div className="main">
         <Home/>
         <About/>
+        <Skills/>
       </div>
     </>
   )
