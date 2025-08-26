9cb57353ea9515af1fed0d0dadaa6e7fa7f24f1b
ShuklaPranav007 <shuklapranav739@gmail.com>
2025-08-27 00:56:45 +0530

    services added

--- portfolio/src/components/services/services.jsx
+++ portfolio/src/components/services/services.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import "./services.css";
 
 const Services = () => {
+  const [toggleState, setToggleState] = useState(0);
+
+  const toggleTab = (index) => {
+    setToggleState(index);
+  };
   return (
     <section className="services section" id="services">
       <h2 className="section__title">Services</h2>
@@ -11,17 +16,35 @@ const Services = () => {
         <div className="services__content">
           <div>
             <i className="uil uil-web-grid services__icon"></i>
-            <h3 className="services__title">Product <br /> Designer </h3>
+            <h3 className="services__title">
+              Product <br /> Designer{" "}
+            </h3>
           </div>
 
-          <span className="services__button">
+          <span
+            className="services__button"
+            onClick={() => {
+              toggleTab(1);
+            }}
+          >
             View More
-            <i className="uil uil-arrow-right services__icon srevices__button-icon"></i>
+            <i className="uil uil-arrow-right services__icon services__button-icon"></i>
           </span>
 
-          <div className="services__modal">
+          <div
+            className={
+              toggleState === 1
+                ? "services__modal active-modal"
+                : "services__modal"
+            }
+          >
             <div className="services__modal-content">
-              <i className="uil uil-times services__modal-close"></i>
+              <i
+                onClick={() => {
+                  toggleTab(0);
+                }}
+                className="uil uil-times services__modal-close"
+              ></i>
 
               <h3 className="services__modal-title">Designer</h3>
               <p className="services__modal-description">
@@ -31,7 +54,7 @@ const Services = () => {
                 at tenetur cumque temporibus.
               </p>
 
-              <ul className="services__modal-services grid">
+              <ul className="services__modal-service grid">
                 <li className="services__modal-services">
                   <i className="uil uil-check-circle services__modal-icon"></i>
                   <p className="services__modal-info">
@@ -72,19 +95,37 @@ const Services = () => {
         <div className="services__content">
           <div>
             <i className="uil uil-arrow services__icon"></i>
-            <h3 className="services__title">Ui/Ux <br /> Designer</h3>
+            <h3 className="services__title">
+              Ui/Ux <br /> Designer
+            </h3>
           </div>
 
-          <span className="services__button">
+          <span
+            className="services__button"
+            onClick={() => {
+              toggleTab(2);
+            }}
+          >
             View More
-            <i className="uil uil-arrow-right services__icon srevices__button-icon"></i>
+            <i className="uil uil-arrow-right services__icon services__button-icon"></i>
           </span>
 
-          <div className="services__modal">
+          <div
+            className={
+              toggleState === 2
+                ? "services__modal active-modal"
+                : "services__modal"
+            }
+          >
             <div className="services__modal-content">
-              <i className="uil uil-times services__modal-close"></i>
+              <i
+                onClick={() => {
+                  toggleTab(0);
+                }}
+                className="uil uil-times services__modal-close"
+              ></i>
 
-              <h3 className="services__modal-title">Designer</h3>
+              <h3 className="services__modal-title">Ui Designer</h3>
               <p className="services__modal-description">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Repellendus aut ut necessitatibus magnam cum ea amet nisi
@@ -92,7 +133,7 @@ const Services = () => {
                 at tenetur cumque temporibus.
               </p>
 
-              <ul className="services__modal-services grid">
+              <ul className="services__modal-service grid">
                 <li className="services__modal-services">
                   <i className="uil uil-check-circle services__modal-icon"></i>
                   <p className="services__modal-info">
@@ -133,19 +174,37 @@ const Services = () => {
         <div className="services__content">
           <div>
             <i className="uil uil-edit services__icon"></i>
-            <h3 className="services__title">Visual <br /> Designer</h3>
+            <h3 className="services__title">
+              Visual <br /> Designer
+            </h3>
           </div>
 
-          <span className="services__button">
+          <span
+            className="services__button"
+            onClick={() => {
+              toggleTab(3);
+            }}
+          >
             View More
-            <i className="uil uil-arrow-right services__icon srevices__button-icon"></i>
+            <i className="uil uil-arrow-right services__icon services__button-icon"></i>
           </span>
 
-          <div className="services__modal">
+          <div
+            className={
+              toggleState === 3
+                ? "services__modal active-modal"
+                : "services__modal"
+            }
+          >
             <div className="services__modal-content">
-              <i className="uil uil-times services__modal-close"></i>
+              <i
+                onClick={() => {
+                  toggleTab(0);
+                }}
+                className="uil uil-times services__modal-close"
+              ></i>
 
-              <h3 className="services__modal-title">Designer</h3>
+              <h3 className="services__modal-title">Visual Designer</h3>
               <p className="services__modal-description">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Repellendus aut ut necessitatibus magnam cum ea amet nisi
@@ -153,7 +212,7 @@ const Services = () => {
                 at tenetur cumque temporibus.
               </p>
 
-              <ul className="services__modal-services grid">
+              <ul className="services__modal-service grid">
                 <li className="services__modal-services">
                   <i className="uil uil-check-circle services__modal-icon"></i>
                   <p className="services__modal-info">
@@ -190,7 +249,6 @@ const Services = () => {
             </div>
           </div>
         </div>
-        
       </div>
     </section>
   );
