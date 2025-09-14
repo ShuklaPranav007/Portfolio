7ff975ae05e0af2083ce8b72e206749a3a47b03b
ShuklaPranav007 <shuklapranav739@gmail.com>
2025-09-14 12:08:57 +0530

    pascal

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import "./App.css";
+
 import Header from "./components/header/Header.jsx";
 import Home from "./components/home/Home.jsx";
 import About from "./components/about/About.jsx";
 import Skills from "./components/skills/Skills.jsx";
 import Qualification from "./components/qualification/Qualification.jsx";
 import Projects from "./components/projects/Projects.jsx";
+import Certificate from "./components/certificates/Certificate.jsx";
 import Contact from "./components/contact/Contact.jsx";
 import Footer from "./components/footer/Footer.jsx";
 import ScrollUp from "./components/scrollup/ScrollUp.jsx";
-import Certificate from "./components/certificates/Certificate.jsx";
 
 function App() {
   return (
